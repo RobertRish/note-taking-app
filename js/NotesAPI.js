58a0cb83e7a1dd49e044cdcd6f4cca150079df35
RobertRish <robert.rish1225@gmail.com>
2022-09-24 13:49:38 -0400

    save note to localStorage
    Multi-line description of commit.
    Feel free to be detailed.

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -9,8 +9,10 @@ export default class NotesAPI {
     static saveNote(noteToSave) {
         const notes = NotesAPI.getAllNotes();
 
-        noteToSave.id = Math.floor(Math.random() * 1000000)
+        noteToSave.id = Math.floor(Math.random() * 1000000);
+        noteToSave.updated = new Date().toISOString();
         notes.push(noteToSave);
+
         localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
     static deleteNote(id) {
